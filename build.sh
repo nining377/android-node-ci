da01334f92035076c9c4f6121431aaaabfde854e
nining377 <nining377@gmail.com>
2022-06-03 15:01:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ download_and_extract() {
 build-android() {
 
   # make sure some functions are available in link stage
-  sed -i "s/.src\/unix\/android-ifaddrs.c.,/'src\/unix\/android-ifaddrs.c','\/src\/unix\/epoll.c',/g" deps/uv/uv.gyp
+  sed -i "s/.src\/unix\/android-ifaddrs.c.,/'src\/unix\/android-ifaddrs.c','src\/unix\/epoll.c',/g" deps/uv/uv.gyp
   
   ./android-configure $ANDROID_NDK_HOME $ANDROID_ABI 23
 
